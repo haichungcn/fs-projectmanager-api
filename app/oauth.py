ea21024927e740ec0463aa10d9c95bc6b521d81d
haichungcn <haichungcn@gmail.com>
2019-11-28 00:44:18 +0700

    change url to netlify

--- app/oauth.py
+++ app/oauth.py
@@ -60,7 +60,7 @@ def facebook_logged_in(blueprint, token):
         token = Token(user_id=current_user.id, uuid=str(uuid.uuid4().hex))
         db.session.add(token)
         db.session.commit()
-    return redirect("https://localhost:3000/?api_key={}".format(token.uuid))
+    return redirect("https://xenodochial-mahavira-452f56/?api_key={}".format(token.uuid))
 
 
 # notify on OAuth provider error
